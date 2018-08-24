e8922ee9d04fe5b88f428a4a87bfd89063dffaec
MoGReM <40706098+MoGReM@users.noreply.github.com>
2018-08-24 07:53:08 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -308,4 +308,12 @@ client.on('ready', () => {
 });
 
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`in ${client.guilds.size} servers `)
+    console.log(`[user] ${client.users.size}`)
+    client.user.setStatus("Idle ")
+});
+
+
 client.login(process.env.BOT_TOKEN);
